20980fb75ce67faec0f4f222476ed35a721908ed
Cook <vincentthomasetude@gmail.com>
2016-11-06 15:10:44 +0100

    Implementation de la Class "Categorie"

---
+++ src/Model/Categorie.java
@@ -0,0 +1,46 @@
+package Model;
+
+
+/**
+ * <h1>Range les {@link Taches} dans des Categories</h1>
+ * Comporte le Titre et une Abreviation de la Categorie
+ *
+ * @author  Vincent THOMAS
+ */
+public class Categorie {
+    /**
+     * Titre de la Categorie
+     */
+    private String titre;
+
+    /**
+     * Abreviation de la Categorie
+     */
+    private String abreviation;
+
+    /**
+     * Constructeur de la class
+     *
+     * @param titre Titre de la Categorie
+     * @param abreviation Abreviation de la Categorie
+     */
+    public Categorie(String titre, String abreviation){
+        this.titre = titre;
+        this.abreviation = abreviation;
+    }
+
+    /**
+     * Setter de l'atribut @link titre
+     * @param titre Titre de la Categorie
+     */
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    /**
+     * @param abreviation Abreviation de la Categorie
+     */
+    public void setAbreviation(String abreviation) {
+        this.abreviation = abreviation;
+    }
+}
