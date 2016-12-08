28268a2300d028e6fad7bb3ab72e090a21b40178
gkueny <gaetankueny@gmail.com>
2016-12-08 09:58:48 +0100

    update build

--- build&Test&Run.sh
+++ build&Test&Run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #compile
-javac -cp ./src:./out:./Junit/junit.jar -d ./out ./src/View/*.java ./src/Controller/*.java ./src/Model/*.java ./test/Model/*.java
+/usr/lib/jvm/java-8-oracle/bin/javac -cp ./src:./out:./Junit/junit.jar -d ./out ./src/View/*.java ./src/Controller/*.java ./src/Model/*.java ./test/Model/*.java
 
 #executez test
 java -cp ./out:./Junit/junit.jar:./out/ Model.AuLongCoursTest
