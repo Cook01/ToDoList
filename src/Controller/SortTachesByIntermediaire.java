ff322ab789901129c819524192c946921153b5f9
gkueny <gaetankueny@gmail.com>
2016-12-10 17:43:26 +0100

    add comment in SortTachesByIntermediaire

--- src/Controller/SortTachesByIntermediaire.java
+++ src/Controller/SortTachesByIntermediaire.java
@@ -6,11 +6,21 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
+/**
+ * Implémentation du trie par échéance intermédiaire
+ */
 class SortTachesByIntermediaire implements SortTaches
 {
 
+    // Comparaisons des echances intermediaire de chaque tache
     private Comparator<Tache> comparator = (left, right) -> left.getDateEcheanceIntermediaire().getTime().compareTo(right.getDateEcheanceIntermediaire().getTime());
 
+    /**
+     * Fonction de trie des taches
+     *
+     * @param allTaches ArrayList de toute les taches
+     * @return ArrayList de taches triées
+     */
     public ArrayList<Tache> sort(ArrayList<Tache> allTaches)
     {
         // Sorting
