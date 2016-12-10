37ca159cfaf6e79862dcd50e43cdbac3dfece270
gkueny <gaetankueny@gmail.com>
2016-12-10 17:44:47 +0100

    add comment in SortTachesByNewest

--- src/Controller/SortTachesByNewest.java
+++ src/Controller/SortTachesByNewest.java
@@ -5,11 +5,20 @@ import java.util.ArrayList;
 
 import Model.*;
 
+/**
+ * Implémentation du trie par date de fin
+ */
 class SortTachesByNewest implements SortTaches
 {
-
+    // Comparaisons des dates de fin de chaque taches
     private Comparator<Tache> comparator = Comparator.comparing(left -> left.getEnd().getTime());
 
+    /**
+     * Fonction de trie des taches
+     *
+     * @param allTaches ArrayList de toute les taches
+     * @return ArrayList de taches triées
+     */
 	public ArrayList<Tache> sort(ArrayList<Tache> allTaches)
 	{
 		// Sorting
