e26e6653ac0a8368cf4347d369a0178bac48e35b
gkueny <gaetankueny@gmail.com>
2016-12-10 17:39:00 +0100

    add comment in BilanListener

--- src/Controller/BilanListener.java
+++ src/Controller/BilanListener.java
@@ -5,20 +5,38 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Date;
 
+/**
+ * Lister of Bilan view
+ */
 public class BilanListener implements ActionListener {
+
 
     private JSpinner beginDate;
     private JSpinner endDate;
 
+    /**
+     *
+     * @param beginDate JSpinner de début du bilan
+     * @param endDate JSpinner de fin du bilan
+     */
     public BilanListener(JSpinner beginDate, JSpinner endDate) {
         super();
         this.beginDate = beginDate;
         this.endDate = endDate;
     }
 
+    /**
+     *
+     * On écoute les action de la vue Bilan
+     *
+     * @param e ActionEvent action performed
+     */
     public void actionPerformed(ActionEvent e) {
+
         switch (e.getActionCommand()) {
+
             case "Générer bilan" :
+                //On appele le Main Controller afin de générer le bilan
                 MainController.generateBilan((Date)this.beginDate.getValue(), (Date)this.endDate.getValue());
                 break;
         }
