7cbe71f9c8821a348f13d0608405cebc08540cc5
gkueny <gaetankueny@gmail.com>
2016-12-11 02:36:24 +0100

    add sciprt

---
+++ build&test&doc.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+#compile
+javac -cp ./src:./out:./Junit/junit.jar -d ./out ./src/View/*.java ./src/Controller/*.java ./src/Model/*.java ./test/Model/*.java
+#executez test
+java -cp ./out:./Junit/junit.jar:./out/ Model.AuLongCoursTest
+java -cp ./out:./Junit/junit.jar:./out/ Model.CategorieTest
+java -cp ./out:./Junit/junit.jar:./out/ Model.PonctuelleTest
+java -cp ./out:./Junit/junit.jar:./out/ Model.TacheTest
+
+#doc
+javadoc -encoding "utf8" -docencoding "utf8" -d doc/ -author ./src/View/*.java ./src/Controller/*.java ./src/Model/*.java ./test/Model/*.java
